e1eed8c859a4408e880dae7762eb251fe06f9ef8
kcsarith <krisnasarith@gmail.com>
2021-04-21 23:27:15 -0700

    rename file

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,16 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-part 'counter_cubit_state.dart';
+part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counterValue: 0));
 
   void increment() {
-    emit(CounterState(counterValue: state.counterValue + 1));
+    emit(CounterState(
+        counterValue: state.counterValue + 1, wasIncremented: true));
   }
 
   void decrement() {
-    emit(CounterState(counterValue: state.counterValue - 1));
+    emit(CounterState(
+        counterValue: state.counterValue - 1, wasIncremented: false));
   }
 }
